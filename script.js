03e2fb51685f918350e717a9299e2c7c1d2e7db0
Nofal Sher <nofalsher7@gmail.com>
2022-01-13 01:04:23 +0500

    Update script.js

--- script.js
+++ script.js
@@ -5,20 +5,30 @@ const count=document.getElementById("count");
 
 const total=document.getElementById("total");
 const movieSelect=document.getElementById("movie");
+
 const ticketPrice=+movieSelect.value;  // We have added a plus sign ahead of movieSelect to actually convert it in to number format from string format
 
 
-container.addEventListener("click",function(e){
+  
+function updateSelectedCount(){
+    const selectedSeats=document.querySelectorAll(".row.seat.selected ")
+    const selectedSeatsCount=selectedSeats.length;
+    // console.log(selectedSeatsCount)
+    count.innerText=selectedSeatsCount;
+    total.innerText=selectedSeatsCount* ticketPrice;
+}
+
+container.addEventListener("click", e=> {  
+    // We have made a function here. In ES6 basically we donot need to make a function 
+// Rather we can write   (e)=>{}
+    
+
    // console.log("e.target"); //e.target tells us that at which seat had we clicked
-if(e.target.classList.contains("seat")&&
+if(e.target.classList.contains("seat") &&
+!e.target.classList.contains("occupied")
 ){
-    console.log(e.target)
-
-
+    e.target.classList.toggle("selected"); // If we click for first time it will be selected and for next click it will be again de selected.
+    updateSelectedCount();
 }
 
 })    //Initialize an event when it is clicked
-
-// We have made a function here. In ES6 basically we donot need to make a function 
-// Rather we can write   (e)=>{}
-
